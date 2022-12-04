93cfc56ed94d6ccdc75c99c8713457ced7c1b01d
Jose Contreras <josedanielcd@hotmail.com>
2022-12-04 10:58:05 -0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -12,21 +12,21 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(),
-      home: const MyHomePage(title: 'EMPLEOSYVAaswdCANTES'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool hovered = false; 
+  bool hovered2 = false;
   int _counter = 0;
 
   void _incrementCounter() {
@@ -38,27 +38,80 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Container(
+              color: Colors.red, 
+              child: const Text(
+                "PROXIMAMENTE",
+                style: TextStyle(fontSize: 60),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Container(
+              color: Colors.red,
+              margin: const EdgeInsets.only(
+                top: 200,
+              ),
+              child: Stack(
+                alignment: Alignment.centerRight,
+                children: <Widget>[
+                  Positioned(
+                    top: 0,
+                    right: 0,
+                    child: MouseRegion(
+                      onEnter: (event) {
+                        setState(() {
+                          hovered = true;
+                        });
+                      },
+                      onExit: (event) {
+                        setState(() {
+                          hovered = false;
+                        });
+                      },
+                      child: AnimatedContainer(
+                        color: Colors.red,
+                        duration: const Duration(milliseconds: 300),
+                        width: hovered
+                            ? MediaQuery.of(context).size.width / 2.5
+                            : MediaQuery.of(context).size.width / 2.7,
+                        child: Image.asset(
+                          "photos/Empleosbaqlogo.jpeg",
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ),
+                  ),
+                  MouseRegion(
+                    onEnter: (event) {
+                      setState(() {
+                        hovered2 = true;
+                      });
+                    },
+                    onExit: (event) {
+                      setState(() {
+                        hovered2 = false;
+                      });
+                    },
+                    child: AnimatedContainer(
+                      color: Colors.red,
+                      duration: const Duration(milliseconds: 300),
+                      width: hovered2
+                          ? MediaQuery.of(context).size.width / 2.5
+                          : MediaQuery.of(context).size.width / 2.7,
+                      child: Image.asset(
+                        "photos/Empleosyvacanteslogo.jpeg",
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
