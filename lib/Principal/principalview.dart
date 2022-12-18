4f3bc20a4d3b7f071f7ea0289be767e2a0df1142
Jose Contreras <josedanielcd@hotmail.com>
2022-12-18 16:40:58 -0500

    update Fade Animation

--- lib/Principal/principalview.dart
+++ lib/Principal/principalview.dart
@@ -1,8 +1,9 @@
+import 'dart:html';
+
 import 'package:empleosyvacantes/Components/components.dart';
 import 'package:empleosyvacantes/constants.dart';
 import 'package:empleosyvacantes/responsive.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 class Principal extends StatefulWidget {
   const Principal({super.key});
@@ -14,13 +15,18 @@ class Principal extends StatefulWidget {
 class _PrincipalState extends State<Principal> with TickerProviderStateMixin {
   late AnimationController controller;
   late Animation<double> animation;
+  late Animation<double> animation2;
+  late Animation<double> animation3;
+
+  Animation<double> animationFade(double startValue) {
+    animation = Tween(begin: 0.0, end: 5.0).animate(CurvedAnimation(parent: controller, curve:   Interval(startValue,1.0,curve: Curves.linear)));
+   return animation;
+   }
 
   @override
   void initState() {
-    controller = AnimationController(
-        vsync: this, duration: const Duration(milliseconds: 1500));
-    animation = Tween(begin: 0.0, end: 1.0).animate(controller);
-
+   controller = AnimationController(
+        vsync: this, duration: const Duration(seconds: 5));
     super.initState();
   }
 
@@ -28,64 +34,64 @@ class _PrincipalState extends State<Principal> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     controller.forward();
     // Responsive responsive = Responsive(context);
-
     Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
       body: Container(
         width: screenSize.width,
-        height: screenSize.height,
-        color: Colors.black,
+         color: Colors.black,
         child: SingleChildScrollView(
-          child: Stack(
+          child: Column(
             children: [
               Container(
-                color: Colors.black,
-                height: screenSize.height,
-                width: screenSize.width,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      "JOFFERT",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: screenSize.width * 0.05,
-                          fontWeight: FontWeight.w900),
-                    ),
-                    Container(
-                        width: screenSize.width * 0.05,
-                        height: screenSize.width * 0.05,
-                        constraints: const BoxConstraints(
-                          maxWidth: 200,
-                          maxHeight: 200,
+                    color: Colors.black,
+                    height: screenSize.height,
+                    width: screenSize.width,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        FadeTransition(
+                          opacity: animationFade(0.0),
+                          child: Container(
+                          color: Colors.transparent,
+                          child: Text(
+                            "JOFFERT",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: screenSize.width * 0.05,
+                                fontWeight: FontWeight.w900),
+                          ),
+                          ),
                         ),
-                        child: const CircularProgressIndicator(
-                          strokeWidth: 8,
-                        ))
+                        FadeTransition(
+                          opacity: animationFade(0.6),
+                          child: Container(
+                          color: Colors.transparent,
+                          child: CircularProgressIndicator()
+                          ),
+                        ),
+                        FadeTransition(
+                          opacity: animationFade(0.3),
+                          child: Container(
+                          color: Colors.transparent,
+                          child: Text(
+                            "Tu mejor opcion para publicar ofertas",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: screenSize.width * 0.02,
+                                fontWeight: FontWeight.w900),
+                          ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+              Row(
+                  children: [
+                    WebsiteCards(photo: empleosyvacantes),
+                    WebsiteCards(photo: empleosbaq)
                   ],
                 ),
-              ),
-              PositionedTransition(
-                rect: RelativeRectTween(
-                  begin: RelativeRect.fromLTRB(
-                      -MediaQuery.of(context).size.width / 2.5,
-                      0,
-                      MediaQuery.of(context).size.width / 1.5,
-                      0),
-                  end: RelativeRect.fromLTRB(
-                      0, 0, MediaQuery.of(context).size.width / 2.5, 0),
-                ).animate(
-                  CurvedAnimation(parent: controller, curve: Curves.bounceOut),
-                ),
-                child: FadeTransition(
-                  opacity: animation,
-                  child: WebsiteCards(photo: empleosbaq),
-                ),
-              ),
-              Positioned(
-                  top: 0,
-                  right: 0,
-                  child: WebsiteCards(photo: empleosyvacantes))
+         
             ],
           ),
         ),
